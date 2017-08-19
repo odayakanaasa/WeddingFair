d8fc3cb65a8c59f53694d56044bca6ee2c25bf5f
SudeshNT <sudeshnt93@gmail.com>
2017-08-19 21:40:50 +0530

    gallery view

--- www/app/modules/shop/controllers/ItemListCtrl.js
+++ www/app/modules/shop/controllers/ItemListCtrl.js
@@ -2,9 +2,124 @@
 
 angular.module('shop.module').controller('ItemListCtrl',ItemListCtrl );
 
-ItemListCtrl.$inject = ['$scope','$state','$rootScope','$mdSidenav','$log'];
+ItemListCtrl.$inject = ['$scope','$state','$rootScope','$filter','$mdSidenav','$log'];
 
-function ItemListCtrl($scope,$state,$rootScope,$mdSidenav,$log) {
+function ItemListCtrl($scope,$state,$rootScope,$filter,$mdSidenav,$log) {
+
+  $scope.isSearchEnabled = false;
+  $scope.bookMarked = false;
+
+  $scope.datapointsList = [
+    { DPNAME: 'Samsung', id: 1 },
+    { DPNAME: 'Android', id: 2 },
+    { DPNAME: 'Apple', id: 3 },
+    { DPNAME: 'Indigo', id: 4 },
+    { DPNAME: 'Phonegap', id: 8 },
+    { DPNAME: 'System', id: 9 },
+    { DPNAME: 'India', id: 5 },
+    { DPNAME: 'Ionic', id: 6 }
+  ];
+  $scope.names=$scope.datapointsList ;
+  $scope.adn = {};
+  $scope.srchchange = function () {
+
+    $scope.names = null;
+    var filtervalue = [];
+    var serachData=$scope.datapointsList;
+    console.log(serachData);
+    for (var i = 0; i <serachData.length; i++) {
+
+      var fltvar = $filter('uppercase')($scope.adn.item);
+      var jsval = $filter('uppercase')(serachData[i].DPNAME);
+
+      if (jsval.indexOf(fltvar) >= 0) {
+        filtervalue.push(serachData[i]);
+      }
+    }
+    // console.log("last");
+    console.log(filtervalue);
+    $scope.names = filtervalue;
+
+  };
+
+  $scope.ressetserach = function () {
+    $scope.adn.item = "";
+    $scope.names =$scope.datapointsList;
+    $scope.isSearchEnabled = false;
+  }
+
+  //auto complete
+
+  // var self = this;
+
+  $scope.simulateQuery = false;
+  $scope.isDisabled    = false;
+
+  // list of `state` value/display objects
+  $scope.states        = loadAll();
+  $scope.querySearch   = querySearch;
+  $scope.selectedItemChange = selectedItemChange;
+  $scope.searchTextChange   = searchTextChange;
+
+  $scope.newState = newState;
+
+  function newState(state) {
+    alert("Sorry! You'll need to create a Constitution for " + state + " first!");
+  }
+
+  // ******************************
+  // Internal methods
+  // ******************************
+
+  /**
+   * Search for states... use $timeout to simulate
+   * remote dataservice call.
+   */
+  function querySearch (query) {
+    var results = query ? $scope.states.filter( createFilterFor(query) ) : $scope.states,
+      deferred;
+    if ($scope.simulateQuery) {
+      deferred = $q.defer();
+      $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+      return deferred.promise;
+    } else {
+      return results;
+    }
+  }
+
+  function searchTextChange(text) {
+    $log.info('Text changed to ' + text);
+  }
+
+  function selectedItemChange(item) {
+    $log.info('Item changed to ' + JSON.stringify(item));
+  }
+
+  /**
+   * Build `states` list of key/value pairs
+   */
+  function loadAll() {
+    var allStates = 'Ampara, Anuradhapura, Badulla, Batticalao, Colombo, Galle, Gampaha, Hambantota, Jaffna, Kalutara, Kandy, Kegalle, Kilinochchi, Kurunegala, Mannar, Matale, Matara, Moneragala, Mullaittivu, Nuwara Eliya, Polonnaruwa, Puttalam, Ratnapura, Trincomalee, Vavuniya';
+
+    return allStates.split(/, +/g).map( function (state) {
+      return {
+        value: state.toLowerCase(),
+        display: state
+      };
+    });
+  }
+
+  /**
+   * Create filter function for a query string
+   */
+  function createFilterFor(query) {
+    var lowercaseQuery = angular.lowercase(query);
+
+    return function filterFn(state) {
+      return (state.value.indexOf(lowercaseQuery) === 0);
+    };
+
+  }
 
   $scope.goHome = function(){
     $state.go('home');
--- www/app/modules/shop/controllers/ServiceProviderCtrl.js
+++ www/app/modules/shop/controllers/ServiceProviderCtrl.js
@@ -2,95 +2,120 @@
 
 angular.module('shop.module').controller('ServiceProviderCtrl',ServiceProviderCtrl );
 
-ServiceProviderCtrl.$inject = ['$scope','$state','$rootScope'];
+ServiceProviderCtrl.$inject = ['$scope','$state','$rootScope','$ionicModal'];
 
-function ServiceProviderCtrl($scope,$state,$rootScope) {
+function ServiceProviderCtrl($scope,$state,$rootScope,$ionicModal) {
     $scope.tabs = [
       {"text" : "Photography"},
       {"text" : "Videography"},
       {"text" : "Preshoot"}
     ];
 
-   var gallery = [
+   $scope.gallery = [
         {
           "imageUrl" : "1.jpg",
+          "index":0,
           "width" : 640,
-          "height" : 960
+          "height" : 960,
+          "src" : "app/modules/shop/img/gallery/1.jpg"
         },
         {
           "imageUrl" : "2.jpg",
+          "index":1,
           "width" : 640,
-          "height" : 960
+          "height" : 960,
+          "src" : "app/modules/shop/img/gallery/2.jpg"
         },
         {
           "imageUrl" : "3.jpg",
+          "index":2,
           "width" : 960,
-          "height" : 640
+          "height" : 640,
+          "src" : "app/modules/shop/img/gallery/3.jpg"
         },
         {
           "imageUrl" : "4.jpg",
+          "index":3,
           "width" : 640,
-          "height" : 960
-        },
-        {
-          "imageUrl" : "4.jpg",
-          "width" : 640,
-          "height" : 960
+          "height" : 960,
+          "src" : "app/modules/shop/img/gallery/4.jpg"
         },
         {
           "imageUrl" : "5.jpg",
+          "index":4,
           "width" : 960,
-          "height" : 640
+          "height" : 640,
+          "src" : "app/modules/shop/img/gallery/5.jpg"
         },
         {
           "imageUrl" : "6.jpg",
+          "index":5,
           "width" : 640,
-          "height" : 960
+          "height" : 960,
+          "src" : "app/modules/shop/img/gallery/6.jpg"
         },
         {
           "imageUrl" : "7.jpg",
+          "index":6,
           "width" : 960,
-          "height" : 640
+          "height" : 640,
+          "src" : "app/modules/shop/img/gallery/7.jpg"
         },
         {
           "imageUrl" : "8.jpg",
+          "index":7,
           "width" : 640,
-          "height" : 960
+          "height" : 960,
+          "src" : "app/modules/shop/img/gallery/8.jpg"
         },
         {
           "imageUrl" : "9.jpg",
+          "index":8,
           "width" : 960,
-          "height" : 600
+          "height" : 600,
+          "src" : "app/modules/shop/img/gallery/9.jpg"
         },
         {
           "imageUrl" : "10.jpg",
+          "index":9,
           "width" : 600,
-          "height" : 960
+          "height" : 960,
+          "src" : "app/modules/shop/img/gallery/10.jpg"
         },
         {
           "imageUrl" : "11.jpg",
+          "index":10,
           "width" : 600,
-          "height" : 960
+          "height" : 960,
+          "src" : "app/modules/shop/img/gallery/11.jpg"
         },
         {
           "imageUrl" : "12.jpg",
+          "index":11,
           "width" : 960,
-          "height" : 600
+          "height" : 600,
+          "src" : "app/modules/shop/img/gallery/12.jpg"
         },
         {
           "imageUrl" : "13.jpg",
+          "index":12,
           "width" : 600,
-          "height" : 960
+          "height" : 960,
+          "src" : "app/modules/shop/img/gallery/13.jpg"
         },
         {
           "imageUrl" : "14.jpg",
+          "index":13,
           "width" : 600,
-          "height" : 960
+          "height" : 960,
+          "src" : "app/modules/shop/img/gallery/14.jpg"
         },
         {
           "imageUrl" : "15.jpg",
+          "index":14,
           "width" : 960,
-          "height" : 640
+          "height" : 640,
+          "src" : "app/modules/shop/img/gallery/15.jpg"
         }
    ];
 
@@ -104,20 +129,20 @@ function ServiceProviderCtrl($scope,$state,$rootScope) {
    function processGallery(){
      var colOneHeight = 0;
      var colTwoHeight = 0;
-     for(var i in gallery){
+     for(var i in $scope.gallery){
         if($scope.processedGallery.col_1.length > 0 && $scope.processedGallery.col_2.length>0){
           colOneHeight = getTotalHeightOfColumn($scope.processedGallery.col_1);
           colTwoHeight = getTotalHeightOfColumn($scope.processedGallery.col_2);
           if(colOneHeight <= colTwoHeight){
-            $scope.processedGallery.col_1.push(gallery[i]);
+            $scope.processedGallery.col_1.push($scope.gallery[i]);
           }else{
-            $scope.processedGallery.col_2.push(gallery[i]);
+            $scope.processedGallery.col_2.push($scope.gallery[i]);
           }
         }else{
           if($scope.processedGallery.col_1.length==0){
-            $scope.processedGallery.col_1.push(gallery[i]);
+            $scope.processedGallery.col_1.push($scope.gallery[i]);
           }else{
-            $scope.processedGallery.col_2.push(gallery[i]);
+            $scope.processedGallery.col_2.push($scope.gallery[i]);
           }
         }
      }
@@ -147,6 +172,29 @@ function ServiceProviderCtrl($scope,$state,$rootScope) {
     swiper.on('onSlideChangeEnd', function (x) {
       console.log('slide end',x.activeIndex);
     });
+  };
+
+  //gallery
+
+  $scope.showImages = function(index) {
+    console.log(index);
+    $scope.activeSlide = index;
+    $scope.showModal('app/modules/shop/templates/gallery-zoomview.html');
+  };
+
+  $scope.showModal = function(templateUrl) {
+    console.log(templateUrl);
+    $ionicModal.fromTemplateUrl(templateUrl, {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+  }
+
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+    $scope.modal.remove()
   };
 
 }
